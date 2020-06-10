bbabae69f71f06ae1b95e8802a4127b3a2a1d12c
darwin09afp <45778547+darwin09afp@users.noreply.github.com>
2020-06-09 20:38:14 -0400

    Template agregado al View Crear reservacion

--- SIREON/Controllers/HomeController.cs
+++ SIREON/Controllers/HomeController.cs
@@ -10,13 +10,13 @@ namespace SIREON.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.title = "Bienvenido";
+            ViewBag.title = "Bienvenido | SIREON";
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.title = "Sobre nosotros";
+            ViewBag.title = "Sobre nosotros | SIREON";
             ViewBag.Message = "Your application description page.";
 
             return View();
@@ -24,14 +24,14 @@ namespace SIREON.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.title = "Contactos";
+            ViewBag.title = "Contactos | SIREON";
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
         public ActionResult Help()
         {
-            ViewBag.title = "Ayuda";
+            ViewBag.title = "Ayuda | SIREON";
             ViewBag.Message = "Your Help page.";
 
             return View();
