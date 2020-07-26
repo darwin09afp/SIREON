1a8e44239fed27091a46be520fd1f9f44e0c9e9d
darwin09afp <45778547+darwin09afp@users.noreply.github.com>
2020-07-26 14:11:01 -0400

    Lista de Invitados a reservacion modificada (aun sin enviar nada a base de datos)

--- SIREON/Controllers/ReservacionesController.cs
+++ SIREON/Controllers/ReservacionesController.cs
@@ -114,6 +114,46 @@ namespace SIREON.Controllers
 
 
 
+
+
+        [HttpPost]
+        public ActionResult SaveOrder2(string IdAspNetUsers, TimeSpan HSolicitada, TimeSpan HEntrada, TimeSpan HSalida, Reservaciones_Usuarios[] reservaciones_Usuarios)
+        {
+            string result = "Error! Datos no completados!";
+            List<Cubiculo> cubs = new List<Cubiculo>();
+            var FechayHora = DateTime.Now;
+            var SelFecha = FechayHora.Date;
+            var SelHora = FechayHora.TimeOfDay;
+            var empleado = db.AspNetUsers.ToList().FirstOrDefault().Id;
+            var disponibilidad = db.Cubiculos.ToList()/*.Where(x => x.Estatus == "Libre")*/.FirstOrDefault().ID_Cubiculo;
+            Reservacione model = new Reservacione();
+            model.ID_Empleado = empleado;
+            model.Fecha = SelFecha;
+            model.ID_Cubiculo = disponibilidad;
+            model.FechaSolicitada = SelFecha;
+            model.HSolicitada = HSolicitada;
+            model.HEntrada = HEntrada;
+            model.HSalida = HSalida;
+            model.Estatus = "Activa";
+            model.IdAspNetUsers = IdAspNetUsers;
+            db.Reservaciones.Add(model);
+            db.SaveChanges();
+
+            foreach (var item in reservaciones_Usuarios)
+            {
+                Reservaciones_Usuarios Res = new Reservaciones_Usuarios();
+                Res.IdAspNetUser = item.IdAspNetUser;
+                Res.IdReservacion = model.ID_Reservacion;
+                db.Reservaciones_Usuarios.Add(Res);
+                db.SaveChanges();
+            }
+
+            result = "Exito! Datos guardados!";
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+
         // GET: Reservaciones1/Create
         public ActionResult Create()
         {
@@ -130,6 +170,36 @@ namespace SIREON.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "ID_Reservacion,ID_Empleado,Fecha,ID_Cubiculo,FechaSolicitada,HSolicitada,HEntrada,HSalida,Estatus,IdAspNetUsers")] Reservacione reservacione)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Reservaciones.Add(reservacione);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID_Cubiculo = new SelectList(db.Cubiculos, "ID_Cubiculo", "Descripcion", reservacione.ID_Cubiculo);
+            ViewBag.IdAspNetUsers = new SelectList(db.AspNetUsers, "Id", "Email", reservacione.IdAspNetUsers);
+            ViewBag.ID_Empleado = new SelectList(db.AspNetUsers, "Id", "Email", reservacione.ID_Empleado);
+            return View(reservacione);
+        }
+
+        // GET: Reservaciones/CreateOp
+        public ActionResult CreateOp()
+        {
+            ViewBag.ID_Cubiculo = new SelectList(db.Cubiculos, "ID_Cubiculo", "Descripcion");
+            ViewBag.IdAspNetUsers = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.ID_Empleado = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+
+        // POST: Reservaciones/CreateOp
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateOp([Bind(Include = "ID_Reservacion,ID_Empleado,Fecha,ID_Cubiculo,FechaSolicitada,HSolicitada,HEntrada,HSalida,Estatus,IdAspNetUsers")] Reservacione reservacione)
         {
             if (ModelState.IsValid)
             {
