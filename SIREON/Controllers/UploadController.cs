7980567b2cc92d52402efa0307b025f3fd492505
darwin09afp <45778547+darwin09afp@users.noreply.github.com>
2020-08-18 03:29:15 -0400

    Mostrar el reglamento

---
+++ SIREON/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIREON.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload  
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if (file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                ViewBag.Message = "El Reglamento fue guardado exitosamente!";
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "No se pudo guardar el Reglamento!";
+                return View();
+            }
+        }
+    }
+}
